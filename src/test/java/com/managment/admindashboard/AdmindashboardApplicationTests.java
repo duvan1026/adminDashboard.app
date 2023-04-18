9301e03d63041719fa1115f70ade89170da36e22
Duvan Castro <duvancastro1026@gmail.com>
2023-04-18 04:25:45 -0500

    add files projects

---
+++ src/test/java/com/managment/admindashboard/AdmindashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managment.admindashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmindashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
