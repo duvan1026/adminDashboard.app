b0a6c9db94abb0640a36696688c2dd29bbf70baf
Duvan Castro <duvancastro1026@gmail.com>
2023-04-18 04:56:15 -0500

    implement in method prueba of List

--- src/main/java/com/managment/admindashboard/controllers/UsuarioController.java
+++ src/main/java/com/managment/admindashboard/controllers/UsuarioController.java
@@ -4,11 +4,13 @@ package com.managment.admindashboard.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
 
     @GetMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    public List<String> prueba(){
+        return List.of("Manzana","Kiwi", "Banana");
     }
 }
