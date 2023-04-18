3a6c81248aa89c7bc2cd8919d091dbb1def2a30b
Duvan Castro <duvancastro1026@gmail.com>
2023-04-18 05:11:16 -0500

    create and implement method getUsuario for the test

--- src/main/java/com/managment/admindashboard/controllers/UsuarioController.java
+++ src/main/java/com/managment/admindashboard/controllers/UsuarioController.java
@@ -1,16 +1,54 @@
 package com.managment.admindashboard.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.managment.admindashboard.models.Usuario;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @GetMapping(value = "prueba")
-    public List<String> prueba(){
-        return List.of("Manzana","Kiwi", "Banana");
+    @GetMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Duvan");
+        usuario.setApellido("Castro");
+        usuario.setEmail("duvancastro1026@gmail.com");
+        usuario.setTelefono("3156802266");
+        return usuario;
     }
+
+//    @PostMapping(value = "usuario")
+//    public Usuario editarUsuario(){
+//        Usuario usuario = new Usuario();
+//        usuario.setNombre("Duvan");
+//        usuario.setApellido("Castro");
+//        usuario.setEmail("duvancastro1026@gmail.com");
+//        usuario.setTelefono("3156802266");
+//        return usuario;
+//    }
+//
+//
+//    @DeleteMapping(value = "usuario")
+//    public Usuario deleteUsuario(){
+//        Usuario usuario = new Usuario();
+//        usuario.setNombre("Duvan");
+//        usuario.setApellido("Castro");
+//        usuario.setEmail("duvancastro1026@gmail.com");
+//        usuario.setTelefono("3156802266");
+//        return usuario;
+//    }
+//
+//
+//    @GetMapping(value = "usuario")
+//    public Usuario Usuario(){
+//        Usuario usuario = new Usuario();
+//        usuario.setNombre("Duvan");
+//        usuario.setApellido("Castro");
+//        usuario.setEmail("duvancastro1026@gmail.com");
+//        usuario.setTelefono("3156802266");
+//        return usuario;
+//    }
 }
