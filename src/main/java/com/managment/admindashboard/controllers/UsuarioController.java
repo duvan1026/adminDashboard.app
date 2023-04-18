4cb987589f2cc39b844ac6aed742b61e0b8f376f
Duvan Castro <duvancastro1026@gmail.com>
2023-04-18 04:41:17 -0500

    implement first method of the test

--- src/main/java/com/managment/admindashboard/controllers/UsuarioController.java
+++ src/main/java/com/managment/admindashboard/controllers/UsuarioController.java
@@ -1,4 +1,14 @@
 package com.managment.admindashboard.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
+
+    @GetMapping(value = "prueba")
+    public String prueba(){
+        return "prueba";
+    }
 }
