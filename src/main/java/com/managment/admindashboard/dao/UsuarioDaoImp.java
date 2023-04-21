b51c7be8b96e2d55718b715b64d765dbb3440dd6
Duvan Castro <duvancastro1026@gmail.com>
2023-04-21 05:10:39 -0500

    implement querry getusuarios

--- src/main/java/com/managment/admindashboard/dao/UsuarioDaoImp.java
+++ src/main/java/com/managment/admindashboard/dao/UsuarioDaoImp.java
@@ -1,2 +1,24 @@
-package com.managment.admindashboard.dao;public class UsuarioDaoImp {
+package com.managment.admindashboard.dao;
+
+import com.managment.admindashboard.models.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Repository
+@Transactional
+public class UsuarioDaoImp implements UsuarioDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Usuario> getUsuarios() {
+        String query = "FROM Usuario"; // se selecciona el nombre de la clase
+        return entityManager.createQuery(query).getResultList();
+    }
 }
