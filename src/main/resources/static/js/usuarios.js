8e6eb3c192d265adea648299b30ac66d277040c1
Duvan Castro <duvancastro1026@gmail.com>
2023-04-21 04:38:27 -0500

    implement dynamo table usuarios

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -15,6 +15,29 @@ async function cargarUsuarios(){
     });
     const usuarios = await request.json();
 
+    let listadoHtml = '';
+
+    for(let usuario of usuarios){
+        let usuarioHtml = '<tr>\n' +
+            '               <td>'+ usuario.id + '</td>\n' +
+            '               <td>' + usuario.nombre + ' ' + usuario.apellido +'</td>\n' +
+            '               <td>' + usuario.email + '</td>\n' +
+            '               <td>' +  usuario.telefono + '</td>\n' +
+            '               <td>\n' +
+            '                   <a href="#" class="btn btn-danger btn-circle btn-sm">\n' +
+            '                   <i class="fas fa-trash"></i>\n' +
+            '                   </a>\n' +
+            '               </td>\n' +
+            '          </tr> ';
+        listadoHtml += usuarioHtml;
+
+    }
+
     console.log(usuarios);
+
+
+
+    document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
+
 
 }
